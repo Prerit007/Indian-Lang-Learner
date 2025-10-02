1821cad53af8ba9165d5f2409ea2bcdeba1ea00e
Prerit007 <75621370+Prerit007@users.noreply.github.com>
2025-10-02 23:26:04 +0530

    Added Text-Speech

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sarvamai import SarvamAI
 from dotenv import load_dotenv
-import os
+import os, base64
 
 load_dotenv()
 api_key = os.getenv("SARVAM_AI")
@@ -42,41 +42,49 @@ LANGUAGES = {
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {
-        "request": request,
-        "languages": LANGUAGES,
-        "original_text": "",
-        "src_selected": "hi-IN",   # default source language
-        "tgt_selected": "en-IN",   # default target language
-        "translated_text": ""
-    })
-
+    return templates.TemplateResponse("index.html", {"request": request, "languages": LANGUAGES})
 
 @app.post("/translate", response_class=HTMLResponse)
-def translate(
-    request: Request,
-    text: str = Form(...),
-    src_lang: str = Form(...),
-    tgt_lang: str = Form(...)
-):
+def translate(request: Request, text: str = Form(...), src_lang: str = Form(...), tgt_lang: str = Form(...)):
     try:
-        response = client.text.translate(
+        # Step 1: Translation
+        translation = client.text.translate(
             input=text,
             source_language_code=src_lang,
             target_language_code=tgt_lang,
-            model="sarvam-translate:v1",
-            speaker_gender="Male"
+            model="sarvam-translate:v1"
         )
-        # SarvamAI returns TranslationResponse object
-        translated_text = getattr(response, "translated_text", str(response))
+        translated_text = translation.translated_text
+
+        # Step 2: TTS
+        tts = client.text_to_speech.convert(
+            text=translated_text,
+            target_language_code=tgt_lang,
+            speaker="anushka",
+            pitch=0,
+            pace=0.9,
+            loudness=1,
+            speech_sample_rate=22050,
+            enable_preprocessing=True,
+            model="bulbul:v2"
+        )
+
+        audio_base64 = "".join(tts.audios)  # if it's a list
+        audio_data_uri = f"data:audio/wav;base64,{audio_base64}"
+
+
+        with open("out.wav", "wb") as f:
+            f.write(base64.b64decode(audio_base64))
+
     except Exception as e:
         translated_text = f"Error: {str(e)}"
+        audio_data_uri = None
 
     return templates.TemplateResponse("index.html", {
         "request": request,
         "languages": LANGUAGES,
         "original_text": text,
-        "src_selected": src_lang,
         "tgt_selected": tgt_lang,
-        "translated_text": translated_text
+        "translated_text": translated_text,
+        "audio_data": audio_data_uri
     })
